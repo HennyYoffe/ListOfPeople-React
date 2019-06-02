9625da69509cef1d912ddba56827ff4f3498d039
Henny Yoffe <hennyy1149@gmail.com>
2019-06-01 23:21:51 -0400

    april 1

--- HW67_AddToTable_May29/ClientApp/src/index.js
+++ HW67_AddToTable_May29/ClientApp/src/index.js
@@ -1,19 +1,63 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import Person from './listofpeople';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+class App extends React.Component {
+    state = {     
+        allPeople: [],        
+            firstName: '',
+            lastName: '',
+            age: 0
+        
+        
+    }
+    addPerson = () => {       
+        const copy = [...this.state.allPeople];
+        const person = {
+            firstName : this.state.firstName,
+            lastName : this.state.lastName,
+            age : this.state.age
+        }
+        copy.push(person);
+        this.setState({ allPeople: copy });
+    }
+    clearTable = () => {
+        this.setState({ allPeople: [] });
+    }
+    onFirstNameChange = e => {
+        this.setState({ firstName: e.target.value });
+    }
+    onLastNameChange = e => {
+        this.setState({ lastName: e.target.value });
+    }
+    onAgeChange = e => {
+        this.setState({ age: e.target.value });
+    }
 
-registerServiceWorker();
+
+
+    render() {
+        return (
+            <div className="container" style={{ marginTop: 40 }}>
+                <Person
+                    allPeople={this.state.allPeople}
+                    clearTable={this.clearTable}
+                    addPerson={this.addPerson}
+                    onFirstNameChange={this.onFirstNameChange}
+                    onLastNameChange={this.onLastNameChange}
+                    onAgeChange={this.onAgeChange} />
+                
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('root'));
+
+//Create a react application that has three textboxes and two buttons(Add and Clear).
+//Beneath that, there should be a table.When the Add button is clicked, the texboxes should clear out,
+//    and the person should be added to the table.When the clear button is clicked, the table should be cleared.
+
+//As an added bonus, see if you can add a delete button on each row, so that when that button is clicked,
+//    only THAT person gets deleted.Good luck!
---
+++ HW67_AddToTable_May29/ClientApp/src/listofpeople.js
@@ -0,0 +1,42 @@
+﻿import React from 'react';
+
+
+export default class Person extends React.Component {  
+
+    
+
+    render() {
+        const { allPeople } = this.props;
+        return (
+            <div className = "row">
+                <input value={this.props.firstName}
+                    type="text"
+                    onChange={this.props.onFirstNameChange} 
+                />
+                <input value={this.props.lastName}
+                    type="text"
+                    onChange={this.props.onLastNameChange}
+                />
+                <input value={this.props.age}
+                    type="text"
+                onChange={this.props.onAgeChange}
+                />
+                <button onClick={this.props.addPerson}>Add</button>
+                <button onClick={this.props.clearTable}>Clear</button>
+                <table className="table table-bordered table-striped">
+                    <th>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Age</td>
+                    </th>
+                    {allPeople.map((i, idx) => <tr key={idx} >
+                        <td>{i.firstName}</td>
+                        <td>{i.lastName}</td>
+                        <td>{i.age}</td>
+                    </tr>
+)}
+                </table>
+            </div>
+        )
+    }
+}
